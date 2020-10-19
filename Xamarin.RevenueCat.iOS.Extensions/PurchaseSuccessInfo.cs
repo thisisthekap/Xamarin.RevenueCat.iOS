04185917f4ce3974d73c36ad46293f83b1b59af6
thisisthekap <c.kapplmueller@me.com>
2020-10-19 15:30:04 +0200

    added ios extensions

---
+++ Xamarin.RevenueCat.iOS.Extensions/PurchaseSuccessInfo.cs
@@ -0,0 +1,17 @@
+﻿using Purchases;
+using StoreKit;
+
+namespace Xamarin.RevenueCat.iOS.Extensions
+{
+    public class PurchaseSuccessInfo
+    {
+        public SKPaymentTransaction Transaction { get; }
+        public RCPurchaserInfo PurchaserInfo { get; }
+
+        public PurchaseSuccessInfo(SKPaymentTransaction transaction, RCPurchaserInfo purchaserInfo)
+        {
+            Transaction = transaction;
+            PurchaserInfo = purchaserInfo;
+        }
+    }
+}
