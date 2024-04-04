e6f3fa74febf19be5a89737eba06c0dbdff96ca0
thisisthekap <christian.kapplmueller@tonestro.com>
2024-04-04 10:11:10 +0200

    fixed tag naming

--- tagit.sh
+++ tagit.sh
@@ -3,9 +3,13 @@ if [ "$#" -ne 1 ]; then
     echo "USAGE: $0 BindingVersionNumber"
     exit 1;
 fi
-
+git pull
 bindingsVersion=$1
 git tag release-bindings-v${bindingsVersion}
 git tag release-extensions-v${bindingsVersion}
+git tag release-ui-bindings-v${bindingsVersion}
+git tag release-ui-extensions-v${bindingsVersion}
 git push origin release-bindings-v${bindingsVersion}
 git push origin release-extensions-v${bindingsVersion}
+git push origin release-ui-bindings-v${bindingsVersion}
+git push origin release-ui-extensions-v${bindingsVersion}
